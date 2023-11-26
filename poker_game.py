810a9ca39c24904eeab9aaab91782ac2b78155ac
Mickeyyayaya <m897420@gmail.com>
2023-11-26 18:25:59 +0800

    center area

--- poker_game.py
+++ poker_game.py
@@ -1,17 +1,20 @@
-import pygetwindow
 import pyautogui
+from selenium import webdriver
+from time import sleep
 from PIL import Image
 
+path = "C:/Users/mickey/sram_transformer/result.png"
+# Open a web browser using Selenium
+driver = webdriver.Edge()
+driver.get('https://laijunbin.github.io/sevens/v1/index.html')
 
-path = "C://Users/mickey/sram_transformer/result.png"
-titles = pygetwindow.getAllTitles()
-
-window = pygetwindow.getWindowsWithTitle('Sevens')[0]
-
-left, top = window.topleft
-right, bottom = window.bottomright
-pyautogui.screenshot(path)
+# Wait for the page to load
+sleep(5)
+# Use pyautogui to interact with elements
+# For example, to click a button located at x=100, y=200
+region = (360,310,1040,560)
+screenshot = pyautogui.screenshot(region=region)
+screenshot.save(path)
 im = Image.open(path)
-#im = im.crop((left,top,right,bottom))
-#im.save(path)
 im.show(path)
+sleep(100)
